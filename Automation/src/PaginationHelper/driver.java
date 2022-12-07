a74f664df8806e489559513615cdf14b4eabfd93
James Ziesel <jziesel@carolina.rr.com>
2022-12-07 11:28:23 -0500

    Placeholder classes created for pagination example

---
+++ Automation/src/PaginationHelper/driver.java
@@ -0,0 +1,8 @@
+package PaginationHelper;
+
+public class driver extends paginationHelper{
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Automation/src/PaginationHelper/paginationHelper.java
@@ -0,0 +1,4 @@
+package PaginationHelper;
+
+public class paginationHelper {
+}
